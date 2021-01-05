29ae4c431144c082ea3d33a8ab123ee9d2bf3e75
Hasan Parasteh <hasanparasteh@gmail.com>
2021-01-05 18:10:09 +0330

    fixed staticfiles_dirs

--- core/settings.py
+++ core/settings.py
@@ -110,7 +110,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "static/dist"]
+STATICFILES_DIRS = ["static/dist"]
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = [BASE_DIR / "media"]
